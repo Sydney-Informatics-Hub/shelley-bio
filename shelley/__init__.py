91447d808c47a4a1580cd5b72e3c2470ecd131bc
fredjaya1 <fredjaya1@gmail.com>
2026-07-08 23:28:23 +0000

    DEV: add dev version

--- shelley/__init__.py
+++ shelley/__init__.py
@@ -5,7 +5,7 @@ This package provides tools for finding, querying, and building modules for
 bioinformatics software from CVMFS repositories.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.2.0-dev"
 
 from .client.cli import main as cli_main
 from .builder.cvmfs_builder import CVMFSModuleBuilder
